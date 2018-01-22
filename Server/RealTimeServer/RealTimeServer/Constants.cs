4a9270d6813cc9259c6fa94af4106ea4ac2a275f
Bailey Miller <bmiller@susquehannasoftware.com>
2018-01-22 16:17:24 -0500

    Server Complete.

---
+++ Server/RealTimeServer/RealTimeServer/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeServer
+{
+    public static class Constants
+    {
+
+        public const string AdminChannel = "admin";
+
+        public const string TaskChannel = "tasks";
+
+    }
+}
